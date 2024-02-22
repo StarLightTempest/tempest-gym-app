43a0435e0515790a4cb47a2c59366458ce23e626
Wilhelm-Arnold Richter <wilhelm-arnold.richter@mbpvonwlmarnold.speedport.ip>
2024-02-23 00:04:07 +0100

    Version:0.3

--- src/DataFixtures/TrainingPlanFixtures.php
+++ src/DataFixtures/TrainingPlanFixtures.php
@@ -17,11 +17,15 @@ class TrainingPlanFixtures extends Fixture
         $trainingPlan1 = new TrainingPlan();
         $trainingPlan1->setName('Plan 1');
         $trainingPlan1->setPersonId($person1);
+        $trainingPlan1->setDescription('Chest and Biceps');
+        $trainingPlan1->setWeekday('Monday');
         $manager->persist($trainingPlan1);
 
         $trainingPlan2 = new TrainingPlan();
         $trainingPlan2->setName('Plan 2');
         $trainingPlan2->setPersonId($person2);
+        $trainingPlan1->setDescription('Back Legs');
+        $trainingPlan1->setWeekday('Thursday');
         $manager->persist($trainingPlan2);
 
         $manager->flush();
